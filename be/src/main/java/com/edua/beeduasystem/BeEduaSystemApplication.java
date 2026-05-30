f02f534fc4fe335ebba42812e71a5eaee1ae6554
konan-1947 <vudinhdang2004tb@gmail.com>
2026-05-31 00:34:06 +0700

    init BE

---
+++ be/src/main/java/com/edua/beeduasystem/BeEduaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.edua.beeduasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeEduaSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeEduaSystemApplication.class, args);
+    }
+
+}
---
+++ be/src/test/java/com/edua/beeduasystem/BeEduaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edua.beeduasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeEduaSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
